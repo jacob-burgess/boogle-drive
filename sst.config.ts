91c150b6e658e5d40839bde8b0842c1b8da22d5c
Jacob Burgess <jacobburgess11@gmail.com>
2025-02-26 12:52:21 -0800

    sst config

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,29 @@
 /// <reference path="./.sst/platform/config.d.ts" />
+import { readdirSync } from "fs";
 
 export default $config({
   app(input) {
     return {
-      name: "boogle-drive",
+      name: "boogle",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: {
+          region: "us-east-1",
+          profile:
+            input?.stage === "production" ? "boogie-production" : "boogie-dev",
+        },
+        cloudflare: true,
+        planetscale: true,
+      },
     };
   },
-  async run() {},
+  async run() {
+    const outputs = {};
+    for (const value of readdirSync("./infra/")) {
+      const result = await import(`./infra/${value}`);
+      if (result.outputs) Object.assign(outputs, result.outputs);
+    }
+    return outputs;
+  },
 });
