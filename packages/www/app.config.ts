bb88bd2f98f5c67d899a36d8428d73d6fe741fd1
Jacob Burgess <jacobburgess11@gmail.com>
2025-02-26 13:35:50 -0800

    aws lambda streaming preset

--- packages/www/app.config.ts
+++ packages/www/app.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    preset: "aws-lambda",
+    awsLambda: {
+      streaming: true,
+    },
+  },
   vite: {
     plugins: [
       tsConfigPaths({
