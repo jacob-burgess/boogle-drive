e4cc54dff1e914cb5a6556d38f87d38e4415c9f6
Jacob Burgess <jacobburgess11@gmail.com>
2025-03-03 13:45:42 -0800

    authgate

--- packages/www/app/routes/drive.tsx
+++ packages/www/app/routes/drive.tsx
@@ -1,6 +1,13 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { auth } from "./-functions";
 
 export const Route = createFileRoute("/drive")({
+  beforeLoad: async ({ context }) => {
+    const subject = await auth();
+    if (!subject) {
+      throw redirect({ to: "/" });
+    }
+  },
   component: RouteComponent,
 });
 
