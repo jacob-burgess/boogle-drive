4e86a08bb1f137453fc5529ec26247a8a956dd4c
Jacob Burgess <jacobburgess11@gmail.com>
2025-03-13 15:26:09 -0700

    organize

--- packages/www/app/components/upload-form.tsx
+++ packages/www/app/components/upload-form.tsx
@@ -1,4 +1,29 @@
-export function UploadForm({ url }: { url: string }) {
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { createServerFn } from "@tanstack/react-start";
+import { useEffect, useState } from "react";
+import { Resource } from "sst";
+
+const getUploadUrl = createServerFn({ method: "GET" }).handler(async () => {
+  const command = new PutObjectCommand({
+    Key: crypto.randomUUID(),
+    Bucket: Resource.Storage.name,
+  });
+  const url = await getSignedUrl(new S3Client({}), command);
+  return url;
+});
+
+export function UploadForm() {
+  const [url, setUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    getUploadUrl().then(setUrl);
+  }, []);
+
+  if (!url) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <form
       onSubmit={async (e) => {
--- packages/www/app/routes/drive/index.tsx
+++ packages/www/app/routes/drive/index.tsx
@@ -1,39 +1,14 @@
-import { createFileRoute } from "@tanstack/react-router";
 import { UploadForm } from "@/components/upload-form";
-import { createServerFn } from "@tanstack/react-start";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { Resource } from "sst";
-import { useState } from "react";
-import { useEffect } from "react";
-
-const getUploadUrl = createServerFn({ method: "GET" }).handler(async () => {
-  const command = new PutObjectCommand({
-    Key: crypto.randomUUID(),
-    Bucket: Resource.Storage.name,
-  });
-  const url = await getSignedUrl(new S3Client({}), command);
-  return url;
-});
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/drive/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [url, setUrl] = useState<string>();
-
-  useEffect(() => {
-    getUploadUrl().then(setUrl);
-  }, []);
-
-  if (!url) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="flex flex-col gap-4 items-center justify-center flex-1">
-      <UploadForm url={url!} />
+      <UploadForm />
     </div>
   );
 }
