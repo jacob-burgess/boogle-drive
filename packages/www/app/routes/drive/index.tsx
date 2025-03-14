a8f016a0da81ea05c6d85e21de31f3f4dde4ae83
Jacob Burgess <jacobburgess11@gmail.com>
2025-03-14 12:55:34 -0700

    basic display files

--- packages/www/app/routes/drive/index.tsx
+++ packages/www/app/routes/drive/index.tsx
@@ -1,14 +1,42 @@
 import { UploadDialog } from "@/components/upload-dialog";
+import { Item } from "@boogle/core/item/item";
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { auth } from "../-functions";
+
+const getFiles = createServerFn({ method: "GET" }).handler(async () => {
+  const subject = await auth();
+  if (!subject) {
+    throw new Error("Unauthorized");
+  }
+  const files = await Item.byOwner(subject.properties.userId);
+  return files;
+});
 
 export const Route = createFileRoute("/drive/")({
   component: RouteComponent,
+  loader: async () => {
+    const files = await getFiles();
+    return { files };
+  },
 });
 
 function RouteComponent() {
+  const data = Route.useLoaderData();
+  const files = data.files;
   return (
-    <div className="flex flex-col gap-4 items-center justify-center flex-1">
-      <UploadDialog />
+    <div className="flex flex-col gap-4 flex-1">
+      <div className="flex flex-col gap-2 p-8">
+        <div className="flex flex-row items-center justify-between gap-2">
+          <h1 className="text-2xl font-bold">Drive</h1>
+          <UploadDialog />
+        </div>
+        <div className="flex flex-col gap-2">
+          {files.map((file) => (
+            <div key={file.id}>{file.name}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
